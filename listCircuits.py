86b01d8fdb1e117547471a056c02d92cdeef5377
Qi Liu <liu4@andrew.cmu.edu>
2018-01-27 13:26:58 -0800

    Using Tor to list circuits.

---
+++ listCircuits.py
@@ -0,0 +1,14 @@
+
+from stem import CircStatus
+from stem.control import Controller
+with Controller.from_port(port = 9051) as controller: controller.authenticate()
+for circ in sorted(controller.get_circuits()): if circ.status != CircStatus.BUILT:
+continue
+print("")
+print("Circuit %s (%s)" % (circ.id, circ.purpose))
+for i, entry in enumerate(circ.path):
+div = '+' if (i == len(circ.path) - 1) else '|' fingerprint, nickname = entry
+desc = controller.get_network_status(fingerprint, None)
+address = desc.address if desc else 'unknown'
+print("%s | %s" % (address,fingerprint)),
+print("%s |" % (controller.get_info("ip-to-country/%s" % address,'unknown'))), print("%s kb/s" % (desc.bandwidth))
